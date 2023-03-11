dbcdaf90dcb705e18a85807c12e7c379e3a83f8f
xxKeefer <xxkeefer.code@gmail.com>
2023-03-12 07:54:39 +1000

    feat: :lipstick: more responsive headers for portable text map

--- src/components/sanity/PortableText/portableComponentsMap.tsx
+++ src/components/sanity/PortableText/portableComponentsMap.tsx
@@ -10,37 +10,37 @@ export const portableComponentsMap = (
   },
   block: {
     h1: ({ children }) => (
-      <h1 className="text-6xl font-black text-white xl:whitespace-nowrap">
+      <h1 className="text-4xl font-black text-white sm:text-5xl md:text-5xl lg:text-6xl xl:whitespace-nowrap">
         {children}
         {anchor && <Anchor href={anchor} />}
       </h1>
     ),
     h2: ({ children }) => (
-      <h2 className="text-5xl font-black text-white xl:whitespace-nowrap">
+      <h2 className="text-3xl font-black text-white sm:text-4xl md:text-4xl lg:text-5xl xl:whitespace-nowrap">
         {children}
         {anchor && <Anchor href={anchor} />}
       </h2>
     ),
     h3: ({ children }) => (
-      <h3 className="text-4xl font-black text-white xl:whitespace-nowrap">
+      <h3 className="text-2xl font-black text-white sm:text-3xl md:text-3xl lg:text-4xl xl:whitespace-nowrap">
         {children}
         {anchor && <Anchor href={anchor} />}
       </h3>
     ),
     h4: ({ children }) => (
-      <h4 className="text-3xl font-black text-white xl:whitespace-nowrap">
+      <h4 className="text-xl font-black text-white sm:text-2xl md:text-2xl lg:text-3xl xl:whitespace-nowrap">
         {children}
         {anchor && <Anchor href={anchor} />}
       </h4>
     ),
     h5: ({ children }) => (
-      <h5 className="text-2xl font-black text-white xl:whitespace-nowrap">
+      <h5 className="text-lg font-black text-white sm:text-xl md:text-xl lg:text-2xl xl:whitespace-nowrap">
         {children}
         {anchor && <Anchor href={anchor} />}
       </h5>
     ),
     h6: ({ children }) => (
-      <h6 className="text-xl font-black text-white xl:whitespace-nowrap">
+      <h6 className="text-md sm:text-md font-black text-white md:text-xl lg:text-xl xl:whitespace-nowrap">
         {children}
         {anchor && <Anchor href={anchor} />}
       </h6>
